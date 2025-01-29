4909d257a87204104c25beee3c77be780fab66ee
Biniam batu <Biniambatu12@gmail.com>
2025-01-29 23:21:28 +0300

    food cart

---
+++ src/components/FoodItem/FoodItem.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { assets } from '../../assets/assets'
+import './FoodItem.css'
+
+const FoodItem = ({item}:any) => {
+
+  const { name, description, price, image } = item
+  const [itemCount, setItemCount] = useState(0)
+
+  return (
+    <div className='food-item'>
+      <div className="food-item-img-container">
+        <img className='food-item-img' src={image} alt="image" />
+        {itemCount ?
+            <div className='food-item-counter'>
+              <img onClick={()=>setItemCount(prev => prev - 1)} src={assets.remove_icon_red} alt="" /> 
+              <p>{itemCount}</p>
+              <img onClick={()=>setItemCount(prev => prev + 1)} src={assets.add_icon_green} alt="" /> 
+            </div>
+           : 
+            <img className='add' onClick={() => setItemCount(prev => prev + 1)} src={assets.add_icon_white} alt="" /> 
+        }
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
+        </div>
+        <p className='food-item-desc'>{description}</p>
+        <p className='food-item-price'>${price}</p>
+      </div>
+    </div>
+  )
+}
+
+export default FoodItem
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
-createRoot(document.getElementById('root')!).render(
+import StoreContextProvider from './context/StoreContext.tsx'
+
+ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-       <App />
+      <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
     </Router>
   </StrictMode>,
 )
